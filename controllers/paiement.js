7d004b3ac26df9acb9bb9e8179ecfb19d6a76460
Stephane Ndour <ndouradefemi@gmail.com>
2019-06-11 17:25:08 +0000

    test paiement

--- controllers/paiement.js
+++ controllers/paiement.js
@@ -52,18 +52,19 @@ exports.payit = (req, res, next) => {
     .then(function (jsonResponse) {
 
     console.log(jsonResponse);
+
     let headers1 = {
         Accept: "text/html",
         'Content-Type': "text/html",
         API_KEY:"bc92c04b8d2cadaa245bad9f4931aee0f6c5735c4ce5b409e5534314d58cc6b0",
         API_SECRET:"51d161b68d4d047c354f8906e2d6c9df72bf33f4758d64d900d8ba595a78cf92",
         };
-        checkoutUrl = "https://preview.payexpresse.com/payment/checkout/"+jsonResponse.message.token;
+        checkoutUrl = "https://preview.payexpresse.com/payment/checkout/"+jsonResponse.token;
         fetch(checkoutUrl, {
             method:'GET',
             headers: headers1
-            }).then((response)=>{
-                console.log(response);
+            }).then((response1)=>{
+                console.log(response1);
             })
     /*
     {
@@ -74,7 +75,7 @@ exports.payit = (req, res, next) => {
     */
 
    res.status(201).json({
-    message: jsonResponse
+    message: response1
 });
     })
 
